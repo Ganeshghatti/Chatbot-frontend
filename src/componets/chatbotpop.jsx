393d406a359443fdbd97b85686a10e683490c78f
Vijay Joshi <bhushanjoshi1714@gmail.com>
2025-03-07 23:40:55 +0530

    Chatbot popup

--- src/componets/chatbotpop.jsx
+++ src/componets/chatbotpop.jsx
@@ -22,7 +22,7 @@ const Chatbotpop = () => {
   return (
     <div>
       <button
-        className="fixed lg:bottom-5 bottom-3 left-5 bg-accent hover:bg-accent-400 text-white rounded-full p-3 lg:p-4 shadow-lg text-xl hover:bg-accent-600 cursor-pointer focus:outline-none z-10"
+        className="fixed lg:bottom-5 bottom-3 right-5 bg-accent hover:bg-accent-400 text-white rounded-full p-3 lg:p-4 shadow-lg text-xl hover:bg-accent-600 cursor-pointer focus:outline-none z-10"
         onClick={toggleChatbot}
       >
         <span className="text-2xl ">
@@ -35,11 +35,10 @@ const Chatbotpop = () => {
       </button>
 
       <div
-        className={`fixed lg:bottom-40 bottom-53 lg:left-18 shadow-lg rounded-lg lg:p-5 p-6 w-92 lg:w-100 lg:h-96 h-95 z-20 transition-transform duration-500 ease-in-out ${
-          isChatbotVisible
-            ? "translate-y-0 opacity-100"
-            : "translate-y-10 opacity-0"
-        }`}
+        className={`fixed lg:bottom-40 bottom-53 lg:right-18 shadow-lg rounded-lg lg:p-5 p-6 w-92 lg:w-100 lg:h-96 h-95 z-20 transition-transform duration-500 ease-in-out ${isChatbotVisible
+          ? "translate-y-0 opacity-100"
+          : "translate-y-10 opacity-0"
+          }`}
       >
         {isChatbotVisible && (
           <Chatbot
